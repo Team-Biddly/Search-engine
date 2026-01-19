7b295f9b09e10b79370a2f4c475f2bd467eb229f
pgc0419 <pgc0419@gmail.com>
2026-01-19 15:12:16 +0900

    refactor: 미사용 hwp api 삭제

--- Demo/HwpToTxt/main.py
+++ Demo/HwpToTxt/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException, File, UploadFile
 from sqlalchemy.orm import Session
 from database import BidNotice, get_db, init_db
-# from hwp_api_converter import HwpApiConverter
 from hwp_olefile_converter import HwpOleFileConverter
 
 app = FastAPI(
@@ -11,11 +10,10 @@ app = FastAPI(
 )
 
 # Init
-# api_converter = HwpApiConverter(api_key="~~~~")
 ole_converter = HwpOleFileConverter()
 init_db()
 
-# upload hwp api
+# upload hwp
 @app.post("/test/upload", tags=["test"])
 async def test_upload(
         file: UploadFile = File(...),
@@ -43,39 +41,6 @@ async def test_upload(
     except Exception as e:
         db.rollback()
         raise HTTPException(status_code=500, detail=str(e))
-
-# # convert hwp api
-# @app.post("/test/api_convert/{notice_id}", tags=["test"])
-# async def test_api_convert(
-#         notice_id: int,
-#         db: Session = Depends(get_db),
-# ):
-#     """
-#     Method: Post
-#     """
-#     document = db.query(BidNotice).filter(BidNotice.id == notice_id).first()
-#     if not document:
-#         raise HTTPException(status_code=404, detail="Notice not found")
-#     if not document.ntceSpecFileNm:
-#         raise HTTPException(status_code=404, detail="NTC Spec file not found")
-#
-#     # HWP to TXT
-#     filename = f"{document.id}.hwp"
-#     success, text = api_converter.hwp_to_txt_api(document.ntceSpecFileNm, filename)
-#     if success:
-#         document.converted_txt = text
-#         document.is_converted = True
-#     else:
-#         document.is_converted = False
-#
-#     db.commit()
-#     db.refresh(document)
-#
-#     return {
-#         "status": "success" if success else "failed",
-#         "document_id": document.id,
-#         "is_converted": document.is_converted,
-#     }
 
 # convert hwp olefile
 @app.post("/test/ole_convert/{notice_id}", tags=["test"])
